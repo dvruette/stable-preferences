0e470fb4a17b26669179c56651e0522dcf30a0d2
Dimitri <dimitrivr@icloud.com>
2023-03-17 19:02:14 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# stable-preferences
+# Stable Preferences
+
+Group project for Generative Visual Models
+
+## Overview
+
+- [🧨 Diffusers](https://huggingface.co/docs/diffusers/index) for diffusion model stuff
+- [Hydra](https://hydra.cc/docs/intro/) for handling configs
+
+## Setup
+
+Create virtual env (alternatively, you can use `conda` if preferred):
+```bash
+python3 -m venv .venv  # create new virtual environment
+source .venv/bin/activate  # activate it
+pip install -r requirements.txt  # install requirements
+pip install -e .  # install current repository in editable mode
+```
+
+If everything worked well, you should be able to execute the example script (might take a while to download the model).
+This should generate "an astronaut riding a horse on mars" and save it in `example.png`.
+```bash
+python stable_preferences/example.py
+```
+
+The script is configured using Hydra, which means you can pass command line arguments as follows (commas need to be escape as `\,`):
+```
+python stable_preferences/example.py prompt="a cute corgi playing the trumpet\, best quality\, trending on artstation\, exceptional detail" neg_prompt="bad art\, worst quality" cfg_scale=10 seed=42
+```
---
+++ stable_preferences/utils.py
@@ -0,0 +1,64 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
+
+def display_images(images, n_cols=4, size=4):
+    n_rows = int(np.ceil(len(images) / n_cols))
+    fig = plt.figure(figsize=(size * n_cols, size * n_rows))
+    for i, img in enumerate(images):
+        ax = fig.add_subplot(n_rows, n_cols, i + 1)
+        ax.imshow(img)
+        ax.axis("off")
+    fig.tight_layout()
+    return fig
+
+
+@torch.no_grad()
+def generate_trajectory(
+    pipe,
+    pos_prompt="a photo of an astronaut riding a horse on mars",
+    neg_prompt="",
+    cfg_scale=5,
+    steps=20,
+    seed=42,
+    show_progress=True,
+    batch_size=1,
+    only_last=False,
+    device=None,
+):  
+    scheduler = pipe.scheduler
+    text_encoder = pipe.text_encoder
+    unet = pipe.unet
+    # vae = pipe.vae
+    tokenizer = pipe.tokenizer
+
+    if seed is not None:
+        torch.manual_seed(seed)
+    z = torch.randn(batch_size, 4, 64, 64, device=device)
+    z = z * scheduler.init_noise_sigma
+
+    scheduler.set_timesteps(steps, device=device)
+    prompt_tokens = tokenizer([pos_prompt] + [neg_prompt], return_tensors="pt", padding=True, truncation=True).to(z.device)
+    prompt_embd = text_encoder(**prompt_tokens).last_hidden_state
+    pos_prompt_embd = prompt_embd[:1]
+    neg_prompt_embd = prompt_embd[-1:]
+    prompt_embd = torch.cat([pos_prompt_embd] * batch_size + [neg_prompt_embd] * batch_size)
+
+    iterator = scheduler.timesteps
+    if show_progress:
+        iterator = tqdm.tqdm(scheduler.timesteps)
+
+    traj = []
+    for _, t in enumerate(iterator):
+        z = scheduler.scale_model_input(z, t)
+        unet_out = unet(torch.cat([z, z], dim=0), t, prompt_embd)
+        noise_cond, noise_uncond = unet_out.sample.chunk(2)
+        
+        noise_pred = noise_uncond + cfg_scale * (noise_cond - noise_uncond)
+
+        z = scheduler.step(noise_pred, t, z).prev_sample
+        if not only_last or t == scheduler.timesteps[-1]:
+            y = pipe.decode_latents(z)
+            traj.append(pipe.numpy_to_pil(y))
+    return traj
