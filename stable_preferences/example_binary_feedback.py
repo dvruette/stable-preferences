40f58d01f23ce08f1792e9c0b9dc465dd8a8557f
jthomy <jthomm@student.ethz.ch>
2023-03-27 19:51:58 +0200

    binary feedback initial approach

---
+++ stable_preferences/example_binary_feedback.py
@@ -0,0 +1,60 @@
+import torch
+import hydra
+from omegaconf import DictConfig
+from diffusers import StableDiffusionPipeline, DPMSolverSinglestepScheduler
+
+from stable_preferences.utils import generate_trajectory, generate_trajectory_with_binary_feedback
+
+MODE = "binary_feedback"
+
+dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+torch.set_default_dtype(dtype)
+
+
+@hydra.main(config_path="configs", config_name="example_binary_feedback", version_base=None)
+def main(ctx: DictConfig):
+    if ctx.device == "auto":
+        device = "mps" if torch.backends.mps.is_available() else "cpu"
+        device = "cuda" if torch.cuda.is_available() else device
+    else:
+        device = ctx.device
+    print(f"Using device: {device}")
+
+    scheduler = DPMSolverSinglestepScheduler.from_pretrained(ctx.model_id, subfolder="scheduler")
+    pipe = StableDiffusionPipeline.from_pretrained(ctx.model_id, scheduler=scheduler, torch_dtype=dtype).to(device)
+
+    print(f"Unet: {sum(p.numel() for p in pipe.unet.parameters()) / 1e6:.0f}M")
+    print(f"VAE: {sum(p.numel() for p in pipe.vae.parameters()) / 1e6:.0f}M")
+    print(f"TextEncoder: {sum(p.numel() for p in pipe.text_encoder.parameters()) / 1e6:.0f}M")
+
+    if MODE!= "binary_feedback":
+        traj = generate_trajectory(
+            pipe,
+            ctx.prompt,
+            ctx.neg_prompt,
+            ctx.cfg_scale,
+            steps=ctx.steps,
+            seed=ctx.seed,
+            only_last=True,
+            device=device,
+        )
+    else:
+        traj = generate_trajectory_with_binary_feedback(
+            pipe,
+            ctx.prompt,
+            ctx.neg_prompt,
+            list(ctx.liked_prompts),
+            list(ctx.disliked_prompts),
+            ctx.cfg_scale,
+            steps=ctx.steps,
+            seed=ctx.seed,
+            only_last=True,
+            device=device,
+        )
+    img = traj[-1][-1]
+    img.save("example.png")
+    img.show()
+
+
+if __name__ == "__main__":
+    main()
--- stable_preferences/utils.py
+++ stable_preferences/utils.py
@@ -16,6 +16,84 @@ def display_images(images, n_cols=4, size=4):
 
 
 @torch.no_grad()
+def generate_trajectory_with_binary_feedback(
+    pipe,
+    pos_prompt="a photo of an astronaut riding a horse on mars",
+    neg_prompt="",
+    liked_prompts=[],
+    disliked_prompts=[],
+    cfg_scale=5,
+    steps=20,
+    seed=42,
+    show_progress=True,
+    batch_size=1,
+    only_last=False,
+    device=None,   
+):
+    scheduler = pipe.scheduler
+    text_encoder = pipe.text_encoder
+    unet = pipe.unet
+    # vae = pipe.vae
+    tokenizer = pipe.tokenizer
+
+    if seed is not None:
+        torch.manual_seed(seed)
+    z = torch.randn(batch_size, 4, 64, 64, device=device)
+    z = z * scheduler.init_noise_sigma
+
+    scheduler.set_timesteps(steps, device=device)
+    if liked_prompts==[]: liked_prompts = [""]
+    if disliked_prompts==[]: disliked_prompts = [""]
+    prompt_tokens = tokenizer([pos_prompt] + [neg_prompt] + liked_prompts + disliked_prompts, return_tensors="pt", padding=True, truncation=True).to(z.device)
+    prompt_embd = text_encoder(**prompt_tokens).last_hidden_state
+    pos_prompt_embd = prompt_embd[:1] # first element
+    neg_prompt_embd = prompt_embd[1:2] # second element
+    liked_prompts_embd = prompt_embd[2:2+len(liked_prompts)]
+    disliked_prompts_embd = prompt_embd[2+len(liked_prompts):]
+    # print("pos_prompt_embd.shape", pos_prompt_embd.shape)
+    # print("liked_prompt_embd.shape", liked_prompts_embd.shape)
+    prompt_embd = torch.cat([pos_prompt_embd] * batch_size + [neg_prompt_embd] * batch_size + [p.unsqueeze(0) for p in liked_prompts_embd for _ in range(batch_size)] + [p.unsqueeze(0) for p in disliked_prompts_embd for _ in range(batch_size)])
+
+    iterator = scheduler.timesteps
+    if show_progress:
+        iterator = tqdm.tqdm(scheduler.timesteps)
+
+    traj = []
+    for _, t in enumerate(iterator):
+        z = scheduler.scale_model_input(z, t)
+        zs = torch.cat((2+len(liked_prompts)+len(disliked_prompts))*[z], dim=0)
+        # print(zs.shape)
+        # print(prompt_embd.shape)
+        assert(zs.shape==(batch_size*(2+len(liked_prompts)+len(disliked_prompts)),4,64,64))
+        unet_out = unet(zs, t, prompt_embd) # layout: pos promt in all samples, neg prompt in all samples, first liked prompt in all samples, second liked prompt in all samples, etc.
+        noise_cond, noise_uncond, noise_liked, noise_disliked = torch.tensor_split(unet_out.sample,[batch_size,2*batch_size,(2+len(liked_prompts))*batch_size])
+
+        # aggregate the noise from the liked and disliked prompts per sample and obtain one mean vector per sample
+        mean_noise_liked = torch.stack(noise_liked.split(batch_size)).mean(axis=0, dtype=torch.float)
+        mean_noise_disliked = torch.stack(noise_disliked.split(batch_size)).mean(axis=0, dtype=torch.float)
+        # print("mean noise liked: ",mean_noise_liked.shape)
+        
+        # cfg_vector = (noise_cond-noise_uncond)
+        # cfg_noramlizer = 1
+        # if mean_noise_liked.shape[0] != 0: # this is the case if there are liked prompts, otherwise the mean vector is empty
+        #     cfg_vector += (mean_noise_liked-noise_cond)
+        #     cfg_noramlizer += 1
+        # if mean_noise_disliked.shape[0] != 0:
+        #     cfg_vector += (noise_cond-mean_noise_disliked)   
+        #     cfg_noramlizer += 1
+        # noise_pred = noise_uncond + cfg_scale * cfg_vector/cfg_noramlizer
+        alpha = 0.25
+        noise_pred = noise_cond + (alpha*(noise_liked-noise_disliked) + (1-alpha)*(noise_cond-noise_uncond))*cfg_scale
+        # print("noise pred: ",noise_pred.shape)
+
+        z = scheduler.step(noise_pred, t, z).prev_sample
+        if not only_last or t == scheduler.timesteps[-1]:
+            y = pipe.decode_latents(z)
+            traj.append(pipe.numpy_to_pil(y))
+    return traj
+    
+
+@torch.no_grad()
 def generate_trajectory(
     pipe,
     pos_prompt="a photo of an astronaut riding a horse on mars",
