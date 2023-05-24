f1f09ba3e36209664309470b7fae49f8c4a42bda
set <set>
2023-05-24 19:56:35 +0200

    add script for getting prompts from HPS

---
+++ stable_preferences/human_preference_dataset/prompts.py
@@ -0,0 +1,70 @@
+import json
+import os
+import random
+import re
+from typing import Optional
+
+def sample_prompts(
+    dataset_path,
+    train_file="preference_train.json",
+    test_file="preference_test.json",
+    split="train",
+    max_num_prompts=-1,
+    seed: Optional[int] = 0,
+    fix_spacing: bool = True,
+):
+    """Sample prompts from the preference dataset.
+
+    Args:
+        dataset_path (str): Path to the preference dataset.
+        train_file (str): Name of the training file.
+        test_file (str): Name of the test file.
+        split (str): Either "train" or "test".
+        max_num_prompts (int): Number of prompts to sample.
+
+    Returns:
+        list: List of sampled prompts.
+    """
+    assert split in ["train", "test"]
+    if split == "train":
+        file_path = os.path.join(dataset_path, train_file)
+    elif split == "test":
+        file_path = os.path.join(dataset_path, test_file)
+    else:
+        raise ValueError("split must be either train or test")
+
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+
+    prompts = []
+    for item in data:
+        prompts.append(item["prompt"])
+
+    if seed is not None:
+        random.seed(seed)
+    random.shuffle(prompts)
+
+    if max_num_prompts < 0:
+        output = prompts
+    output = prompts[:max_num_prompts]
+
+    if fix_spacing:
+        numbers_regex = r"((?<=[0-9])|(?<=[0-9]\.)|(?<=\b[b-zA-Z])) ((?=[0-9])|(?=[a-zA-Z]\b)|(?=(th|st|nd|rd)\b))"
+        output = [re.sub(numbers_regex, "", prompt) for prompt in output]
+        output = [re.sub(r" / ", "/", prompt) for prompt in output]
+        output = [re.sub(r"\( ", "(", prompt) for prompt in output]
+        output = [re.sub(r" \)", ")", prompt) for prompt in output]
+        output = [re.sub(r" - ", "-", prompt) for prompt in output]
+        output = [re.sub(r" :", ":", prompt) for prompt in output]
+        output = [re.sub(r" _ ", "_", prompt) for prompt in output]
+    return output
+
+
+if __name__ == "__main__":
+    prompts = sample_prompts(
+        "stable_preferences/human_preference_dataset/dataset",
+        split="test",
+        max_num_prompts=10,
+        seed=None,
+    )
+    print(prompts)
